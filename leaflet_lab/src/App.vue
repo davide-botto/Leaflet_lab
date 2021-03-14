cfcdeb2b79e7f4c08709482f4fb5250573d76a65
Davide Botto <davidebotto.pro@gmail.com>
2021-03-14 15:24:21 +0100

    Start drawing figure

--- leaflet_lab/src/App.vue
+++ leaflet_lab/src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <div id="app">
-    <l-map :zoom="zoom" :center="center" style="height: 500px; width: 100%">
+    <l-map
+      ref="map"
+      :zoom="zoom"
+      :center="center"
+      style="height: 500px; width: 100%"
+    >
       <l-tile-layer :url="url" :attribution="attribution" />
     </l-map>
   </div>
 </template>
 
 <script>
+/* eslint-disable no-unused-vars */
+import L from "leaflet";
+import LDraw from "leaflet-draw";
+/* eslint-enable no-unused-vars */
+import "leaflet-draw/dist/leaflet.draw.css";
 import { LMap, LTileLayer } from "vue2-leaflet";
-
 export default {
   name: "App",
   components: {
@@ -22,7 +31,65 @@ export default {
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      map: null,
+      featureGroup: []
     };
+  },
+  mounted() {
+    console.log(LDraw);
+    this.$nextTick(() => {
+      this.map = this.$refs.map.mapObject;
+      // Tell leaflet that the map is exactly as big as the image
+      this.map.setMaxBounds(this.bounds);
+      // Leaflet Draw
+      // Add new FeatureGroup from leaflet for Draw objects on map
+
+      this.featureGroup = new window.L.FeatureGroup().addTo(this.map);
+
+      const drawControl = new window.L.Control.Draw({
+        position: "topright",
+        draw: {
+          polyline: {
+            allowIntersection: false,
+            showArea: true,
+          },
+          polygon:  {
+            allowIntersection: false,
+            drawError: {
+              color: 'orange',
+              timeout: 2000,
+              message: '<strong>Not allowed!<strong>',
+            },
+          },
+          rectangle: false,
+          circle: false,
+          marker: false,
+        },
+        edit: {
+          featureGroup: this.featureGroup,
+          remove: true,
+          edit: {
+            // Set color and fill for edited element
+            selectedPathOptions: {
+              color: '#000',
+              fillColor: '#000',
+            },
+          },
+        }
+      });
+      this.map.addControl(drawControl);
+      // const editableLayers = new window.L.FeatureGroup().addTo(this.map);
+      this.map.on(window.L.Draw.Event.CREATED, (e) => {
+        // const type = e.layerType;
+        const layer = e.layer;
+        console.log(layer);
+        const coords = layer._latlngs[0].map(objCoordinates => [objCoordinates.lng, objCoordinates.lat] );
+        console.log("Coordinates ", coords);
+        // Do whatever else you need to. (save to db, add to map etc)
+        this.featureGroup.addLayer(layer);
+      });
+      
+    });
   },
 };
 </script>
